3751eb38dd35f5961a46f86b47847790ec493673
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2017-08-20 15:39:59 -0500

    Adding files

--- README.md
+++
@@ -1,1 +0,0 @@
-# React_And_redux
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p1>This application uses React, Redux, React Router and a variety other helpful libraries
+        </p1>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
